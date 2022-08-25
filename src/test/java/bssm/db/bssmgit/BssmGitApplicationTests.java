82f479af29567a86c69cdbf43e0927d168d4cdbb
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2022-08-25 10:28:26 +0900

    SET :: Project Settings

---
+++ src/test/java/bssm/db/bssmgit/BssmGitApplicationTests.java
@@ -0,0 +1,13 @@
+package bssm.db.bssmgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BssmGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
