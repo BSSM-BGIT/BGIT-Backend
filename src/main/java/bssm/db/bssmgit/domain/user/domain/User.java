89a5693f008b286ab254b4cf63e2387f3708bea2
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2022-09-19 21:21:40 +0900

    FEAT :: User 엔티티에 BOJ 에 관한 컬럼 추가

--- src/main/java/bssm/db/bssmgit/domain/user/domain/User.java
+++ src/main/java/bssm/db/bssmgit/domain/user/domain/User.java
@@ -53,9 +53,21 @@ public class User {
 
     private String img;
 
+    // solvedCount - 사용자가 푼 문제 수
+    private long solvedCount;
+
+    // exp - 사용자가 여태까지 획득한 경험치량
+    private long exp;
+
+    // tier - Bronze V를 1, Bronze IV를 2, ...,
+    // Ruby I을 30, Master를 31로 표현하는 사용자 티어
+    private long tier;
+
+    // maxStreak - 최대 연속 문제 풀이일 수
+    private long maxStreak;
+
     @Builder
-    public User(Long id, String password, Role role, String email, int studentGrade, int studentClassNo, int studentNo, String name, String bsmToken, String githubId, int commits, String githubMsg) {
-        this.id = id;
+    public User(String password, Role role, String email, int studentGrade, int studentClassNo, int studentNo, String name, String bsmToken, String githubId, int commits, String githubMsg, String img, long solvedCount, long exp, long tier, long maxStreak) {
         this.password = password;
         this.role = role;
         this.email = email;
@@ -67,6 +79,11 @@ public class User {
         this.githubId = githubId;
         this.commits = commits;
         this.githubMsg = githubMsg;
+        this.img = img;
+        this.solvedCount = solvedCount;
+        this.exp = exp;
+        this.tier = tier;
+        this.maxStreak = maxStreak;
     }
 
     // auth
