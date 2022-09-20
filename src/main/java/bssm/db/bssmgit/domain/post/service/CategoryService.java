676a7f37fec9aea1c5091bdb37d389ab23cc90d6
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2022-09-20 20:16:54 +0900

    FEAT :: CategoryService

---
+++ src/main/java/bssm/db/bssmgit/domain/post/service/CategoryService.java
@@ -0,0 +1,38 @@
+package bssm.db.bssmgit.domain.post.service;
+
+import bssm.db.bssmgit.domain.post.entity.Category;
+import bssm.db.bssmgit.domain.post.entity.Post;
+import bssm.db.bssmgit.domain.post.entity.repository.CategoryRepository;
+import bssm.db.bssmgit.domain.user.domain.User;
+import bssm.db.bssmgit.domain.user.repository.UserRepository;
+import bssm.db.bssmgit.global.config.security.SecurityUtil;
+import bssm.db.bssmgit.global.exception.CustomException;
+import bssm.db.bssmgit.global.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Service
+public class CategoryService {
+
+    private final UserRepository userRepository;
+    private final CategoryRepository categoryRepository;
+
+    @Transactional
+    public Long createCategory(Post post, String category) {
+        User user = userRepository.findByEmail(SecurityUtil.getLoginUserEmail())
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_LOGIN));
+
+        Category ca = categoryRepository.save(
+                Category.builder()
+                        .name(category)
+                        .build());
+
+        ca.confirmManagerPost(post);
+        ca.confirmUser(user);
+
+        return ca.getId();
+    }
+}
