7f8a9e2521e75af3e4567009ee282f188f9d96e9
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2022-09-27 14:55:03 +0900

    REFACTOR :: RequestParam -> HttpServletRequest

--- src/main/java/bssm/db/bssmgit/domain/user/web/api/AuthApiController.java
+++ src/main/java/bssm/db/bssmgit/domain/user/web/api/AuthApiController.java
@@ -7,7 +7,6 @@ import bssm.db.bssmgit.domain.user.web.dto.response.GitLoginResponseDto;
 import bssm.db.bssmgit.domain.user.web.dto.response.RandomCodeResponseDto;
 import bssm.db.bssmgit.domain.user.web.dto.response.TokenResponseDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,39 +21,33 @@ public class AuthApiController {
     private final BojService bojService;
 
     @PostMapping("/auth/oauth/bsm")
-    @ResponseStatus(HttpStatus.OK)
     public TokenResponseDto loginBsm(HttpServletRequest request) throws IOException {
         return authService.bsmLogin(request.getHeader("authCode"));
     }
 
-    @PostMapping("/login/oauth/github")
-    @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<GitLoginResponseDto> loginGit(@RequestParam String code) throws IOException {
-        GitLoginResponseDto loginResponse = authService.gitLogin(code);
+    @PostMapping("/auth/oauth/github")
+    public ResponseEntity<GitLoginResponseDto> loginGit(HttpServletRequest request) throws IOException {
+        GitLoginResponseDto loginResponse = authService.gitLogin(request.getHeader("code"));
         return ResponseEntity.ok().body(loginResponse);
     }
 
     @GetMapping("/boj/random")
-    @ResponseStatus(HttpStatus.OK)
-    public RandomCodeResponseDto getRandomCode(@RequestParam String bojId) {
-        return bojService.getRandomCode(bojId);
+    public RandomCodeResponseDto getRandomCode(HttpServletRequest request) {
+        return bojService.getRandomCode(request.getHeader("code"));
     }
 
     @PostMapping("/auth/boj")
-    @ResponseStatus(HttpStatus.OK)
     public BojAuthenticationResultResDto bojAuthentication() throws IOException {
         return bojService.matchedCode();
     }
 
     @PutMapping("/refresh")
-    @ResponseStatus(HttpStatus.OK)
     public TokenResponseDto getNewAccessToken(HttpServletRequest request) {
         String refreshToken = request.getHeader("REFRESH-TOKEN");
         return authService.getNewAccessToken(refreshToken);
     }
 
     @DeleteMapping("/logout")
-    @ResponseStatus(HttpStatus.OK)
     public void logout(HttpServletRequest request) {
         authService.logout(request.getHeader("ACCESS-TOKEN"));
     }
