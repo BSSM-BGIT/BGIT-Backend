0372ee306e3dd10ea458e05a0eb4edf9890a74c0
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2022-10-26 08:41:45 +0900

    REFACTOR :: 스케줄러 조정, 상수 값들을 상수 전용 클래스로 분리

--- src/main/java/bssm/db/bssmgit/global/util/Constants.java
+++ src/main/java/bssm/db/bssmgit/global/util/Constants.java
@@ -5,5 +5,5 @@ public class Constants {
     public static final String githubUrl = "https://github.com/";
     public static final String regexForCommit = "<h2 class=\"f4 text-normal mb-2\">";
     public static final String every5minutes = "0 0/5 * * * ?";
-    public static final String every50minutes = "0 0/38 * * * ?";
+    public static final String every50minutes = "0 0/50 * * * ?";
 }
