108955807f97151d51a7784679f49fe07183dcf2
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2022-10-25 15:53:39 +0900

    FIX :: 한명당 신고가 여러번씩 가능한 문제, 스케줄러 설정

--- src/main/java/bssm/db/bssmgit/domain/user/service/ImaginaryNumberService.java
+++ src/main/java/bssm/db/bssmgit/domain/user/service/ImaginaryNumberService.java
@@ -22,15 +22,11 @@ public class ImaginaryNumberService {
     private final UserFacade userFacade;
     private final ImaginaryNumberFacade imaginaryNumberFacade;
 
-    // 1. 한 사람당 한번씩 신고할 수 있음(신고 횟수는 무제한) -> done
-    // 2. 2주 이내에 받은 신고가 3개 이상이면 허수 등록 -> done
-    // 3. 등록된 신고는 2주마다 사라짐
-    // 4. 2주가 지나면 다시 같은사람 신고 가능
     public void reportUser(Long userId) {
         User user = userFacade.getCurrentUser();
         boolean isAlreadyReportedUserExists = imaginaryNumberFacade.findAll()
                 .stream()
-                .anyMatch(imaginaryNumber -> imaginaryNumber.getUser().equals(user));
+                .anyMatch(imaginaryNumber -> Objects.equals(imaginaryNumber.getUser().getId(), user.getId()));
 
         if (isAlreadyReportedUserExists) {
             throw new CustomException(ErrorCode.DONT_REPORT_USER);
@@ -40,30 +36,27 @@ public class ImaginaryNumberService {
         imaginaryNumberFacade.save(imaginaryNumberRequestDto.toImaginaryNumber());
     }
 
-    @Scheduled(cron = "0 */1 * * * *") // TODO: REFACTOR
+    @Scheduled(cron = "0 0 0/1 * * *") // TODO: REFACTOR
     public void updateImaginaryNumberUser() {
         ArrayList<Long> userIds = new ArrayList<>();
         imaginaryNumberFacade.findAll()
                 .forEach(imaginaryNumber -> userIds.add(imaginaryNumber.getReportedUserId()));
 
         ArrayList<User> users = new ArrayList<>();
-        int cnt;
         for (Long userId : userIds) {
-            cnt = 0;
-            for (Long id : userIds) {
-                if (Objects.equals(userId, id)) cnt++;
-                if (cnt >= 3) {
-                    User user = userFacade.findById(id);
-                    user.updateImaginary();
-                    users.add(user);
-                }
+            long count = userIds.stream()
+                    .filter(id -> Objects.equals(id, userId))
+                    .count();
+            if (count > 2) {
+                User user = userFacade.findById(userId);
+                user.updateImaginary();
             }
         }
 
         userFacade.saveAll(users);
     }
 
-    @Scheduled(cron = "0 */1 * * * *") // TODO: REFACTOR
+    @Scheduled(cron = "0 0 0/1 * * *") // TODO: REFACTOR
     public void removeOldReport() {
         imaginaryNumberFacade.findAll()
                 .forEach(imaginaryNumber -> {
