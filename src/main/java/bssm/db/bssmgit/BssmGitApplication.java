b66a68bc91423583a07f32057d5ff838cdf05ec9
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2022-11-09 10:41:00 +0000

    faet(bgit): convention

--- src/main/java/bssm/db/bssmgit/BssmGitApplication.java
+++ src/main/java/bssm/db/bssmgit/BssmGitApplication.java
@@ -1,15 +1,20 @@
 package bssm.db.bssmgit;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@ConfigurationPropertiesScan
 @EnableScheduling
 @EnableJpaAuditing
+@RequiredArgsConstructor
 @SpringBootApplication
 public class BssmGitApplication {
 	public static void main(String[] args) {
+
 		SpringApplication.run(BssmGitApplication.class, args);
 	}
 
